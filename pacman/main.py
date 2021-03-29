f78bc08c6a36c85b4438470bd0f4b4725c178f72
Jason C.H <ctrysbita@outlook.com>
2021-03-29 12:37:38 +0800

    Add game state

--- pacman/main.py
+++ pacman/main.py
@@ -1,8 +1,17 @@
+from enum import Enum
+
 import pygame
 
 import colors
 import util
 from level import Level
+
+
+class GameState(Enum):
+    WELCOME = 0
+    PLAYING = 1
+    WIN = 2
+    OVER = 3
 
 
 class Game:
@@ -12,15 +21,35 @@ class Game:
 
         pygame.mixer.init()
         pygame.mixer.music.load('res/bgm.ogg')
-        pygame.mixer.music.play(-1, 0.0)
 
         pygame.font.init()
         self.font_small = pygame.font.Font('res/alger.ttf', 18)
         self.font_big = pygame.font.Font('res/alger.ttf', 24)
+        self.font_title = pygame.font.Font('res/alger.ttf', 72)
 
         self.clock = pygame.time.Clock()
+        self.state = GameState.WELCOME
 
-    def run(self):
+    def welcome(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit(0)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        self.state = GameState.PLAYING
+                        return
+
+            self.screen.fill(colors.WHITE)
+
+            welcome_title = self.font_title.render('Pac Man', True, colors.BLACK)
+            self.screen.blit(welcome_title, (150, 100))
+
+            pygame.display.flip()
+
+    def play(self):
+        pygame.mixer.music.play(-1, 0.0)
         level = Level()
         wall_sprites = level.setup_walls(colors.SKYBLUE)
         gate_sprites = level.setup_gate(colors.WHITE)
@@ -29,7 +58,7 @@ class Game:
         super_food_sprites = level.setup_super_food(colors.YELLOW,
                                                     colors.WHITE)
         path_data = level.setup_path_data()
-        
+
         score = 0
         movement = util.Vector2.zero()
 
@@ -71,8 +100,18 @@ class Game:
             ghost_sprites.draw(self.screen)
             food_sprites.draw(self.screen)
 
+            score_text = self.font_small.render("Score: %s" % score, True, colors.RED)
+            self.screen.blit(score_text, [10, 10])
+
             pygame.display.flip()
             self.clock.tick(30)
+
+    def run(self):
+        while True:
+            if self.state == GameState.WELCOME:
+                self.welcome()
+            else:
+                self.play()
 
 
 if __name__ == '__main__':
